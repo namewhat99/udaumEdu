4b983e9ae9cd6df0add18b08a0d29532548615a4
namewhat99 <leegh963@naver.com>
2023-02-08 13:10:56 +0900

    5th commit

--- routers/index.js
+++ routers/index.js
@@ -8,7 +8,6 @@ import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node'
 import filestore from "session-file-store"
 import _ from 'lodash'
-import {LocalStorage} from "node-localstorage"
 
 const router = express.Router()
 const __dirname = path.resolve()
@@ -31,28 +30,33 @@ app.use(
 )
 
 router.get('/', homeHandler)
+// 홈 페이지 이동
 
 router.get('/myPage',  async (req, res) => {
-
+    // 마이페이지로 이동하면 아이디에 따라 db를 읽고 그 데이터들을 가져옵니다
     await db.read()
-
+    // 현재 서버에서 로그인 한 아이디별로 db를 불러오는 방법을 찾는중입니다. "2637823050"는 임의의 값 입니다"
     return res.render('mypage_list.html', {da : db.data["2637823050"]})
     
 })
 
-router.get('/myPage', myPageHandler)
 router.get('/afterLogin', loginHandler)
+//로그인 이후 화면 , "~Handler들은 전부 페이지 렌더링"
 router.get('/makeContents1', contentsHandler)
+// 콘텐츠 만들기의 첫번쩨 화면입니다 , 오지선다 / 수수께끼 / 스무고개 중 택하는 페이지.
 router.get('/makeContents2', contentsHandler2)
+// 콘텐츠 색상 선택 페이지
 router.get('/makeContents3_1', contentsHandler3_1)
+// "/makeContents1 에서 오지선다를 선택했을때 넘어가는 페이지"
 router.get('/makeContents3_2', contentsHandler3_2)
+// "/makeContents1 에서 수수께끼를 선택했을때 넘어가는 페이지"
 router.get('/makeContents3_3', contentsHandler3_3)
-router.get('/myPageTest', myPageTest)
+// "/makeContents1 에서 스무고개를 선택했을때 넘어가는 페이지"
 
 
 router.post('/makeContents3_1', async (req , res) => {
-    // makeContents3_1 에서 컨텐츠 작성이 끝나면 routers/db.json에 만든 컨텐츠를 저장합니다
-
+    // 오지선다 콘텐츠 입력 후 post 요청 , db에 저장
+    
     var a = 0;
     
     const Id = req.body.id
@@ -62,19 +66,19 @@ router.post('/makeContents3_1', async (req , res) => {
     _(db.data).forEach(function(n){
         a += _.size(n)
     })
-    
+    // 지금 제작된 콘텐츠가 몇번째 콘텐츠인지 req.body 에 저장
     req.body.count = a + 1
 
     if(!db.data[Id]) db.data[Id] = [];
-    
+    // 현재 db에 그 Id를 key로 갖는 쌍이 없으면 만든다
     db.data[Id].push(req.body)
-
+    // db.data[Id]에 정보를 저장
     db.write()
 
 })
 
 router.post('/makeContents3_2', async (req,  res) => {
-
+    // 수수께끼 콘텐츠 입력 후 post 요청 , db에 저장
     var a = 0;
     const Id = req.body.id
 
@@ -83,18 +87,18 @@ router.post('/makeContents3_2', async (req,  res) => {
     _(db.data).forEach(function(n){
         a += _.size(n)
     })
-
+    // 지금 제작된 콘텐츠가 몇번째 콘텐츠인지 req.body 에 저장
     req.body.count = a + 1
 
     if(!db.data[Id]) db.data[Id] = [];
-    
+    // 현재 db에 그 Id를 key로 갖는 쌍이 없으면 만든다
     db.data[Id].push(req.body)
-
+    // db.data[Id]에 정보를 저장
     db.write()
 })
 
 router.post('/makeContents3_3', async(req,  res) => {
-
+    // 스무고개 콘텐츠 입력 후 post 요청 , db에 저장
     var a = 0;
     const Id = req.body.id
 
@@ -104,19 +108,13 @@ router.post('/makeContents3_3', async(req,  res) => {
     _(db.data).forEach(function(n){
         a += _.size(n)
     })
-     
+     // 지금 제작된 콘텐츠가 몇번째 콘텐츠인지 req.body 에 저장
     if(!db.data[Id]) db.data[Id] = [];
     
     db.data[Id].push(req.body)
-
+    // db.data[Id]에 정보를 저장
     db.write()
 })
 
-router.post('/', (req, res) => {
-    
-    const isLoggedin = req.body.isLoggedin
-    const Id = req.body.id
-
-})
 
 export default router
